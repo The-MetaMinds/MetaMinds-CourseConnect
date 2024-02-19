ec24608016c9380149137c624fb76cf58f1473bc
david <dauphindavlon@gmail.com>
2024-02-19 16:41:59 -0500

    added registration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Registration from './components/registration';
 function App() {
   return (
     <div className="App">
       <h3>This is Metamind site</h3>
+      <Registration />
     </div>
   );
 }
---
+++ src/components/registration.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './registration.css';
+
+const Registration = () => {
+  return (
+    <div className='registration'>registration</div>
+  )
+}
+
+export default Registration
