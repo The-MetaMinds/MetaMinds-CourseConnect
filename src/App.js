afa5231cbe26ddd56662e750aa771c5deac4dc1e
pmansfield0622 <pdm81@scarletmail.rutgers.edu>
2024-02-19 18:07:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="App">
       <h3>This is Metamind site</h3>
+<<<<<<< Updated upstream
       <h4>Hi</h4>
+=======
+      <h4>Hello!</h4>
+>>>>>>> Stashed changes
       <Registration />
       <h3>Test</h3>
     </div>
