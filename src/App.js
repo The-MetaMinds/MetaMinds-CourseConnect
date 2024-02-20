37a9d5714cbb4b49c9aba53dcd98cc9d3f09efac
pmansfield0622 <159803183+pmansfield0622@users.noreply.github.com>
2024-02-20 15:48:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <Registration />
       <h2> Gus JS </h2>
       <h3>Test</h3>
-      <h3>Peters Test</h3>
+      <h3>Peter's Test</h3>
       <h4>Discord Test 2</h4>
       <h5>Edward's Test</h5>
       <h4>Gus js 2</h4>
