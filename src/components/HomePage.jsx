14a0da05cc7c2ffdad02cdc18554d354c081c122
gg684 <gg684@scarletmail.rutgers.edu>
2024-02-24 13:17:47 -0500

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,33 +3,38 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <div style={{ backgroundColor: 'red', height: '50px', width: '100%', position: 'fixed', top: 0 }}>Red Box Header</div>
+      <div style={{ backgroundColor: 'red', height: '60px', width: '100%', position: 'fixed', top: 0 }}>Red Box Header</div>
       
       <div>HomePage</div>
 
 
 
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <br/>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex' }}>
 
       {/* Mathmatics Button */}
-      <br></br> <br></br>
-      <button style={{ fontSize: '29px', backgroundColor: 'rgb(72, 174, 248)', color: 'black' }}>Mathmatics</button>
+      <button style={{ fontSize: '40px', backgroundColor: 'rgb(72, 174, 248)', color: 'black', marginRight: '30px' }}>Mathmatics</button>
 
       {/* Computer Science Button */}
-      <br></br> <br></br>
-      <button style={{ fontSize: '29px', backgroundColor: 'rgb(209, 203, 181)', color: 'black' }}>Computer Science</button>
+      <button style={{ fontSize: '40px', backgroundColor: 'rgb(209, 203, 181)', color: 'black', marginRight: '30px' }}>Computer Science</button>
 
       {/* History Button */}
-      <br></br> <br></br>
-      <button style={{ fontSize: '29px', backgroundColor: 'rgb(220, 252, 130)', color: 'black' }}>History</button>
+      <button style={{ fontSize: '40px', backgroundColor: 'rgb(220, 252, 130)', color: 'black', marginRight: '30px' }}>History</button>
 
+      </div>
       </div>
 
 
+      <br/><br/><br/>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex' }}>
 
       {/* Criminal Justice Button */}
-      <br></br> <br></br>
-      <button style={{ fontSize: '29px', backgroundColor: 'rgb(218, 75, 221)', color: 'black' }}>Criminal Justice</button>
+      <button style={{ fontSize: '40px', backgroundColor: 'rgb(218, 75, 221)', color: 'black', marginRight: '30px' }}>Criminal Justice</button>
+
+      </div>
+      </div>
 
 
       <br></br>
