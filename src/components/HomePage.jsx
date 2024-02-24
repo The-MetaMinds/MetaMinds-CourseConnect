52c94f2c5b132f804a102feb21a4af7e879e01c2
gg684 <gg684@scarletmail.rutgers.edu>
2024-02-23 22:42:18 -0500

    Update HomePage.jsx
    I added buttons, though I can't see these changes deployed, I will find a way to make it accessible via the registration page for testing.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,8 +6,18 @@ const HomePage = () => {
       <div style={{ backgroundColor: 'red', height: '50px', width: '100%', position: 'fixed', top: 0 }}>Red Box Header</div>
       
       <div>HomePage</div>
+
+      {/* Mathmatics Button */}
+      <br></br>
+      <button style={{ fontSize: '22px', backgroundColor: 'blue', color: 'black' }}>Mathmatics</button>
+
+      {/* Computer Science Button */}
+      <br></br>
+      <button style={{ fontSize: '22px', backgroundColor: 'grey', color: 'black' }}>Mathmatics</button>
       
       <input type="text" placeholder="Type your response here" />
+
+      <button>Submit</button>
 
     </div>
   )
