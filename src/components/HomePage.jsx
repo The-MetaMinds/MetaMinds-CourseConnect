5fdc618aae9dd14702199e3f6f67a4fa27c2a1d8
gg684 <gg684@scarletmail.rutgers.edu>
2024-02-23 21:59:18 -0500

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <div style={{ backgroundColor: 'red', height: '50px', width: '100%', position: 'fixed', top: 0 }}>Red Box Header</div>
+      
+      <div>HomePage</div>
+      
+      <input type="text" placeholder="Type your response here" />
+
+    </div>
   )
 }
 
