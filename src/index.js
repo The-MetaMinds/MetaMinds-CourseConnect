70d8d68ab6ecd167a724d231aaaf5b868b328a5a
pmansfield0622 <pdm81@scarletmail.rutgers.edu>
2024-02-25 01:03:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -38,4 +24,45 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Sets Database
+const database = getDatabase(app);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
+
+
+
+//Template Function for Registration to save to Databse
+
+function registerUser() {
+  var name = document.getElementsById('name').value
+  var email = document.getElementsById('email').value
+  var password = document.getElementsById('password').value
+
+  database.ref('users/' + name).set({
+    name : name,
+    email : email,
+    password : password,
+  })
+//This can be changed to "Successfully Registered!" but for now this is just a template.
+  alert('Saved')
+
+}
+
+
+
+
