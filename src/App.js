169ff8d4b8e3c602f2de11a46de5ec0e5fcb7e8f
mvyrax <mvd74@scarletmail.rutgers.edu>
2024-02-25 13:26:09 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ function App() {
     <div className="App">
       <h3>This is Metamind site</h3>
       <Registration />
-      <h2> Gus JS </h2>
-      <h3>Test</h3>
-      <h3>Peter's Test</h3>
-      <h4>Discord Test 2</h4>
-      <h5>Edward's Test</h5>
-      <h5> Kenneth slay </h5>
-      <h5>Mayra's</h5>
+     
 
       <HomePage />
 
