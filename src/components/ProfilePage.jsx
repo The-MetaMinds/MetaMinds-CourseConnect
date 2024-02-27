2d61251a4a3d46f0edaba99fe7249c52333641cc
Edward Castillo <134175077+EdwardC2000@users.noreply.github.com>
2024-02-27 12:38:26 -0500

    Added Default Profile Picture

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -17,33 +17,36 @@ const ProfilePage = () => {
   return (
     <div className="profile-container">
       <h2 className="profile-heading">User Profile</h2>
+      <div className="profile-picture">
+        <img src="defaultProfilePic.jpg" alt="Default Profile" />
+      </div>
       <div className="profile-info">
         <div className="info-item double-column">
-          <label htmlFor="firstName">First Name</label>
+          <label htmlFor="firstName">First Name:</label>
           <input type="text" id="firstName" className="textbox" value={user.firstName} readOnly />
         </div>
         <div className="info-item double-column">
-          <label htmlFor="lastName">Last Name</label>
+          <label htmlFor="lastName">Last Name:</label>
           <input type="text" id="lastName" className="textbox" value={user.lastName} readOnly />
         </div>
         <div className="info-item">
-          <label htmlFor="email">Scarletmail</label>
+          <label htmlFor="email">Email:</label>
           <input type="email" id="email" className="textbox" value={user.email} readOnly />
         </div>
         <div className="info-item">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password:</label>
           <input type="password" id="password" className="textbox" value={user.password} readOnly />
         </div>
         <div className="info-item">
-          <label htmlFor="major">Major</label>
+          <label htmlFor="major">Major:</label>
           <input type="text" id="major" className="textbox" value={user.major} readOnly />
         </div>
         <div className="info-item">
-          <label htmlFor="contactNumber">Contact Number</label>
+          <label htmlFor="contactNumber">Contact Number:</label>
           <input type="text" id="contactNumber" className="textbox" value={user.contactNumber} readOnly />
         </div>
         <div className="info-item double-column">
-          <label htmlFor="coursesCompleted">Courses Completed</label>
+          <label htmlFor="coursesCompleted">Courses Completed:</label>
           <div className="textbox">
             <ul>
               {user.coursesCompleted.map((course, index) => (
@@ -53,7 +56,7 @@ const ProfilePage = () => {
           </div>
         </div>
         <div className="info-item double-column">
-          <label htmlFor="openToTutoring">Open to Tutoring</label>
+          <label htmlFor="openToTutoring">Open to Tutoring:</label>
           <input type="text" id="openToTutoring" className="textbox" value={user.openToTutoring ? 'Yes' : 'No'} readOnly />
         </div>
       </div>
