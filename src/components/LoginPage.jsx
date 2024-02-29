530af9f4eb70d0707dba429c1ce89257c34e8610
Kenneethh <117687335+Kenneethh@users.noreply.github.com>
2024-02-29 14:42:09 -0500

    Creating the Login Page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import './registration.css';
+import React from 'react';
+import './LoginPage.css'; // Make sure to create an appropriate CSS file for styling
 
-const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
+const LoginBox = () => (
+  <div className="login-box">
+    <div className="options">
+      <button className="google-btn">Sign In with Google</button>
+      <button className="signup-btn">Sign Up</button>
+    </div>
+    <h2>Login</h2>
+    <input type="text" placeholder="ScarletMail" />
+    <input type="password" placeholder="Password" />
+    <button>Sign In</button>
+  </div>
+);
 
-    
+const LoginPage = () => (
+  <div className="login-page">
+    <div className="container">
+      <LoginBox />
+    </div>
+  </div>
+);
 
-  )
-}
-
-export default LoginPage
+export default LoginPage;
