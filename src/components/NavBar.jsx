381c234c770b04c3b31bb88e361402bc74cfbf56
Kenneethh <117687335+Kenneethh@users.noreply.github.com>
2024-04-09 11:31:38 -0400

    pushing the logo onto the site

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,36 @@
-
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import './Navbar.css'; // Assuming you have a CSS file for styling
+import logo from './logo.png';
 
-const Navbar = ({userId}) => {
+const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
-      <nav className="navbar-container">
-        <div className="navbar-brand">
-          <Link to="/" className="logo">Logo</Link>
-        </div>  
-        <div className={`navbar-menu ${menuOpen ? 'active' : ''}`}>
-          <ul>
-            <li><Link to={`/profile/${userId}`}>Profile</Link></li>
-            <li><Link to="/registration">Registration</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-        </div>
-        <button className="menu-toggle" onClick={toggleMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </nav>
-    
+    <nav className="navbar-container">
+      <div className="navbar-brand">
+        <Link to="/" className="logo">
+          <img src={logo} alt="logo" /> {/* Use the imported logo */}
+        </Link>
+      </div>
+      <div className={`navbar-menu ${menuOpen ? 'active' : ''}`}>
+        <ul>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/class">Class</Link></li>
+          <li><Link to="/registration">Registration</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </div>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </nav>
   );
 };
 
