3a1bf238d67abd88270e9046274821a1fd2cfcc5
gg684 <gg684@scarletmail.rutgers.edu>
2024-04-10 22:54:12 -0400

    Removed bloating code

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -6,91 +6,6 @@
 
 */
 
-
-
-/*
-
-import React from 'react';
-import './LoginPage.css'; // Make sure to create an appropriate CSS file for styling
-import {Link, useNavigate} from "react-router-dom"
-import { useState } from 'react';
-import useAuth from '../hooks/useAuth';
-
-
-const LoginBox = () => {
-
-  const navigate = useNavigate();
-  const {login} = useAuth();
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const email = formData.get('email');
-    const password = formData.get('password');
-    try {
-      await login(email, password);
-      // Redirect or perform other actions upon successful login
-      navigate('/')
-    } catch (error) {
-      // Handle login error
-    }
-  };
-
-  
-  return (
-  <div className="login-box">
-    <div className="options">
-      <button className="google-btn">Sign In with Google</button>
-      <Link to="/registration"><button className="signup-btn">Sign Up</button></Link>
-    </div>
-    <h2>Login</h2>
-    <form onSubmit={handleSubmit}>
-      <input onChange={handleChange} type="text" name="email" value={formData.email} placeholder="ScarletMail" />
-      <input onChange= {handleChange} type="password" name="password" value={formData.password} placeholder="Password" />
-      <button type='submit'>Sign In</button>
-    </form>
-  </div>
-  )};
-
-const LoginPage = () => (
-  <div className="login-page">
-    <div className="container">
-      <LoginBox />
-    </div>
-  </div>
-);
-
-export default LoginPage;
-
-*/
-
-
-
-
-/*
-import React from 'react';
-import './LoginPage.css'; // Make sure to create an appropriate CSS file for styling
-import {Link, useNavigate} from "react-router-dom"
-import { useState } from 'react';
-import useAuth from '../hooks/useAuth';
-
-*/
-
-
-
 import React from 'react';
 import './LoginPage.css'; // Make sure to create an appropriate CSS file for styling
 import { Link, useNavigate } from "react-router-dom";
@@ -134,7 +49,7 @@ const LoginBox = () => {
       } else if (error.response && error.response.status === 404) {
         setErrorMessage('The email you entered is not connected to an account.');
       } else {
-        // Handle other errors
+      // Handle other errors
         console.error('Login error:', error);
       }
     }
